db8b69dc6799a6b0fe50d7b6a4e2ebbc03cfce99
German Trushkov <germantrushkov@gmail.com>
2023-03-04 10:44:02 +0200

    add files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<Form title="Список задач"></Form>
+</template>
+
+<script>
+	import Form from './components/Form.vue'
+	
+	export default{
+		components: {
+			Form
+		},
+	};
+</script>
+
+<style lang="css">
+	@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;700&display=swap');
+	*{
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		user-select: none;
+		outline: none;
+		font-family: 'Comfortaa';
+	}
+
+	body{background: #fcfcfc;}
+</style>
